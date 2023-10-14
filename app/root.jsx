81290be3d15c455988e57e8ffb1b190837c6e8ee
KishoNakamura <aidanbau_code@outlook.com>
2023-10-14 13:21:42 -0600

    Adding global CSS, Normalize, Google Font, favicon CrunchyRoll, favicon Remix deleted

--- app/root.jsx
+++ app/root.jsx
@@ -7,9 +7,38 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import rootStyles from "./styles/root.css";
+import faviconCrunchyLogo from "../public/img/favicon-32x32-CrunchyLogo.png"
 
 export const links = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { 
+    rel: "stylesheet",
+    href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+  },
+  {
+    rel: "preconnect",
+    href: "https://fonts.googleapis.com"
+  },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "true"
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+  },
+  {
+    rel: "stylesheet",
+    href: rootStyles
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: faviconCrunchyLogo
+  },
 ];
 
 export default function App() {
