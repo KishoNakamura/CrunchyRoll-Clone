880d10b6fe5d415615a04e10f690afde2b754770
KishoNakamura <aidanbau_code@outlook.com>
2023-10-21 15:51:48 -0600

    Changes in the list names

--- app/components/headerLinksExplore.jsx
+++ app/components/headerLinksExplore.jsx
@@ -4,11 +4,11 @@ export default function HeaderLinksExplore() {
   return (
     <>
       <li><Link to={"/videos/popular"}>Popular</Link></li>
-      <li><Link to={"/videos/news"}>New Releases</Link></li>
+      <li><Link to={"/videos/news"}>New</Link></li>
       <li><Link to={"/videos/alphabetical"}>Alphabetical</Link></li>
       <li><Link to={"/videos/simulcasts"}>Simulcast Season</Link></li>
       <li><Link to={"/videos/simulcasts-calendar"}>Release Calendar</Link></li>
-      <li><Link to={"/videos/events-music"}>Music Videos and Concerts</Link></li>
+      <li><Link to={"/videos/events-music"}>Music Videos & Concerts</Link></li>
     </>
   )
 }
